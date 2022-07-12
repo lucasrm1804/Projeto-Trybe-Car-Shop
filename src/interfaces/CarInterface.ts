9c6c1057ad76dfc508dba6c7f8a50f6191a3b5b5
Lucas Monteiro <lucasribeirom@hotmail.com>
2022-07-12 17:18:00 -0300

    Feat - Req3: Cria interface car

---
+++ src/interfaces/CarInterface.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+import { vehicleSchema } from './VehicleInterface';
+
+const CarSchema = vehicleSchema.extend({
+  doorsQty: z.number().min(2).max(4),
+  seatsQty: z.number().min(2).max(7),
+});
+
+export type Car = z.infer<typeof CarSchema>;
