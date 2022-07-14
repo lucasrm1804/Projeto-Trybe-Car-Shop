a67e22d8c556965589fb33eb6fd0b45094e1fb0a
Lucas Monteiro <lucasribeirom@hotmail.com>
2022-07-14 01:47:07 -0300

    implementa test da service

--- src/tests/unit/services/service.test.ts
+++ src/tests/unit/services/service.test.ts
@@ -1,2 +1,37 @@
-// import { expect } from 'chai';
-// import mongoose from 'mongoose';
+import CarService from '../../../services/CarService';
+import { expect } from 'chai';
+import sinon, { SinonStub } from 'sinon';
+import { Model } from 'mongoose';
+
+import { validCar } from '../../../../__tests__/utils/CarsMock';
+
+describe('Testes para camada Service', () => {
+  describe('testa create da Service', () => {
+    beforeEach(() => {
+      sinon.stub(Model, "create").resolves(validCar);
+    });
+    afterEach(() => {
+      (Model.create as SinonStub).restore();
+    })
+    it('cria novo carro no Banco', async () => {
+      const carService = new CarService();
+      const createdCar = await carService.create(validCar);
+      expect(createdCar).to.be.deep.equal(validCar)
+    })
+  }); 
+
+    describe('testa get da service', () => {
+      beforeEach(() => {
+        sinon.stub(ser, "create").resolves(validCar);
+      });
+      afterEach(() => {
+        (Model.create as SinonStub).restore();
+      })
+      it('retorna todos os carros', async () => {
+        const carService = new CarService();
+        const getAll = await carService.read();
+        expect(getAll).to.be.deep.equal(validCar)
+      })
+
+  })
+})
